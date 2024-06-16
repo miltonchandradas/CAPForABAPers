3c9a37116d7e27fc30f328e9eee3a57a0160a217
Milton Chandradas <milton.chandradas@sap.com>
2024-06-16 05:12:32 +0000

    initial commit...

---
+++ srv/service.js
@@ -0,0 +1,13 @@
+const cds = require("@sap/cds");
+
+module.exports = async (srv) => {
+    const { SalesOrders } = srv.entities;
+
+    // connect to S/4HANA
+    const S4_Service = await cds.connect.to("SalesOrderA2X");
+    srv.on(READ, SalesOrders, async (req) => {
+        return await S4_Service.send({
+            query: req.query
+        });
+    })
+}
